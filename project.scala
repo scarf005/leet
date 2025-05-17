b013ea49791d025dd0f922e489da260855352777
scarf <greenscarf005@gmail.com>
2025-05-18 00:07:28 +0900

    build: bump scala version

--- project.scala
+++ project.scala
@@ -1,5 +1,5 @@
-//> using option -feature -deprecation
-//> using options -experimental -language:experimental.betterFors
+//> using scala 3.7.0
+//> using option -feature -deprecation -preview
 
 //> using toolkit 0.7.0
-//> using dep org.scalameta::munit:1.1.0
+//> using dep org.scalameta::munit:1.1.1

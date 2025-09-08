d6b4d0f0c3909068b7ec27a4c92d933674f59285
scarf <greenscarf005@gmail.com>
2025-09-09 01:09:23 +0900

    build: add scalacheck

--- project.scala
+++ project.scala
@@ -4,3 +4,5 @@
 //> using toolkit 0.7.0
 //> using dep org.scalameta::munit:1.1.1
 //> using dep com.lihaoyi::upickle:4.3.0
+//> using dep org.scalameta::munit-scalacheck:1.1.0
+//> using dep org.scalacheck::scalacheck:1.19.0

b1350b3f32fbc9130fca3ec380a612e1bad791f8
scarf <greenscarf005@gmail.com>
2025-09-11 20:36:25 +0900

    build: bump versions

--- project.scala
+++ project.scala
@@ -1,8 +1,8 @@
-//> using scala 3.7.2
+//> using scala 3.7.3
 //> using option -feature -deprecation -preview
 
 //> using toolkit 0.7.0
 //> using dep org.scalameta::munit:1.1.1
-//> using dep com.lihaoyi::upickle:4.3.0
+//> using dep com.lihaoyi::upickle:4.3.2
 //> using dep org.scalameta::munit-scalacheck:1.2.0
 //> using dep org.scalacheck::scalacheck:1.19.0

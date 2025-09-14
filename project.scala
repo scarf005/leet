3db77b04ccf621d5934e83b3752bfe5ea6c8cb9f
scarf <greenscarf005@gmail.com>
2025-09-14 11:22:04 +0900

    build: bump munit (1.1.1 -> 1.2.0)

--- project.scala
+++ project.scala
@@ -2,7 +2,7 @@
 //> using option -feature -deprecation -preview
 
 //> using toolkit 0.7.0
-//> using dep org.scalameta::munit:1.1.1
+//> using dep org.scalameta::munit:1.2.0
 //> using dep com.lihaoyi::upickle:4.3.2
 //> using dep org.scalameta::munit-scalacheck:1.2.0
 //> using dep org.scalacheck::scalacheck:1.19.0

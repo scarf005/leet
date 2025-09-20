9ec7e0fb35bbebbf0ab2fb5f66d498df9618a659
scarf <greenscarf005@gmail.com>
2025-09-20 17:27:00 +0900

    feat: 0003

--- 0003.longest-substring-without-repeating-characters.scala
+++ 0003.longest-substring-without-repeating-characters.scala
@@ -1,17 +1,18 @@
 package leet.`0003`
 
+import collection.mutable.Set
+
 object Solution:
     def lengthOfLongestSubstring(s: String): Int =
         var left = 0
-        val seen = scala.collection.mutable.Set.empty[Char]
-        var maxLen = 0
-        for c <- s do
+        val seen = Set.empty[Char]
+        s.foldLeft(0) { (maxLen, c) =>
             while seen(c) do
                 seen.remove(s(left))
                 left += 1
             seen.add(c)
-            maxLen = math.max(maxLen, seen.size)
-        maxLen
+            math.max(maxLen, seen.size)
+        }
 
 import munit.FunSuite
 
@@ -20,8 +21,8 @@ class Suite extends FunSuite:
 
     List(
       (input = "abcabcbb", expected = 3),
-        (input = "bbbbb", expected = 1),
-        (input = "pwwkew", expected = 3),
+      (input = "bbbbb", expected = 1),
+      (input = "pwwkew", expected = 3),
     ).foreach { case (input, expected) =>
         test(s"lengthOfLongestSubstring($input)"):
             assertEquals(lengthOfLongestSubstring(input), expected)

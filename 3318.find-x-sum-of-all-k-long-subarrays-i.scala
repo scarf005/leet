7d885ce9900d56044bb98a0694ae6f1892cacc69
scarf <greenscarf005@gmail.com>
2025-11-04 21:51:17 +0900

    feat: 3318

--- 3318.find-x-sum-of-all-k-long-subarrays-i.scala
+++ 3318.find-x-sum-of-all-k-long-subarrays-i.scala
@@ -1,4 +1,5 @@
 package leet.`3318`
+import scala.collection.mutable.PriorityQueue
 
 object Solution:
     def findXSum(nums: Array[Int], k: Int, x: Int): Array[Int] =
@@ -17,6 +18,7 @@ class Suite extends FunSuite:
     List(
       (nums = "[1,1,2,2,3,4,2,3]", k = 6, x = 2, output = "[6,10,12]"),
       (nums = "[3,8,7,8,7,5]", k = 2, x = 2, output = "[11,15,15,15,12]"),
+      (nums = "[1,4,3,5]", k = 4, x = 2, output = "[9]"),
     ).foreach { case (nums, k, x, expected) =>
         test(s"findXSum($nums, $k, $x) = $expected"):
             assertEquals(findXSum(read[Array[Int]](nums), k, x).toSeq, read[Seq[Int]](expected))
